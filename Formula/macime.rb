51464a1ab4b22cfc336633bed1fe559985ccdfbe
riodelphino <ryo.heavenlysky@gmail.com>
2026-01-09 04:39:19 +0900

    fix: sha256

--- Formula/macime.rb
+++ Formula/macime.rb
@@ -2,7 +2,7 @@ class Macime < Formula
   desc "Fast macOS IME switcher CLI"
   homepage "https://github.com/riodelphino/macime"
   url "https://github.com/riodelphino/macime/archive/refs/tags/v2.2.0.tar.gz"
-  sha256 "9d735becb328de48575225a24ae9cd01c56e01732a6ceb7a3dd8d8e5e4b6014e"
+  sha256 "e16ea3d746ff086a286831bb4e76c743587bf2a742e92db180ae8fdc68745511"
   license "MIT"
 
   depends_on :macos
