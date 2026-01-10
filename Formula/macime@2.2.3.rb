b64e27f8445b28f0d4f6b7b844715a18b3f3b5e6
riodelphino <ryo.heavenlysky@gmail.com>
2026-01-10 20:44:47 +0900

    upd: macime v2.2.4

--- Formula/macime@2.2.3.rb
+++ Formula/macime@2.2.3.rb
@@ -4,7 +4,6 @@ class Macime < Formula
   url "https://github.com/riodelphino/macime/archive/refs/tags/v2.2.3.tar.gz"
   sha256 "c236d1c6252a832159c2ceadaa75ff119812001895828bfbb9215c961b2670c0"
   license "MIT"
-
   depends_on :macos
 
   def install
