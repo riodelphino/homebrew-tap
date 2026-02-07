100179b50ad5d50a62fbd365dffc1e0f90c6e239
riodelphino <ryo.heavenlysky@gmail.com>
2026-02-07 16:13:02 +0900

    macime 3.6.0

--- Formula/macime@3.6.0.rb
+++ Formula/macime@3.6.0.rb
@@ -1,4 +1,4 @@
-class Macime < Formula
+class MacimeAt360 < Formula
   desc "Fast IME switcher for macOS"
   homepage "https://github.com/riodelphino/macime"
   url "https://github.com/riodelphino/macime/archive/refs/tags/v3.6.0.tar.gz"
